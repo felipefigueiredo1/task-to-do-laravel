0d953df0edda0353924daaffb3751de4bd8ad8f3
Felipe Figueiredo <felipefigueiredosal@gmail.com>
2025-07-29 21:28:43 -0300

    refactor: adding a request class to use both create and update form inputs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -43,33 +44,14 @@ Route::get('/task/{task}', function (Task $task) {
     return view('show', ['task' => $task]);
 })->name('tasks.show');
 
-Route::post('/tasks', function(Request $request) {
-   $data = $request->validate([
-       'title' => 'required|max:255',
-       'description' => 'required',
-       'long_description' => 'required',
-   ]);
+Route::post('/tasks', function(TaskRequest $request) {
+    $task = Task::create($request->validated());
 
-   $task = new Task();
-   $task->title = $data['title'];
-   $task->description = $data['description'];
-   $task->long_description = $data['long_description'];
-   $task->save();
-
-   return redirect(route('tasks.show', ['task' => $task]))->with('success', 'Task created successfully!');
+    return redirect(route('tasks.show', ['task' => $task]))->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-Route::put('/tasks/{task}', function(Request $request, Task $task){
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
-
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
+Route::put('/tasks/{task}', function(TaskRequest $request, Task $task){
+    $task->update($request->validated());
 
     return redirect(route('tasks.show', ['task' => $task]))->with('success', 'Task updated successfully!');
 })->name('tasks.update');
